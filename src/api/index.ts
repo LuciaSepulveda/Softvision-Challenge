fab4bc85b87ed3cd642a7b9fd118404b1a7afcf9
LuciaSepulveda <luciabsep18@gmail.com>
2021-08-30 12:09:20 -0300

    update

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,26 +1,9 @@
 import {Candidate} from "../types/candidate"
 
-const candidates = new Request("./src/api/candidates.json")
+import candidates from "./candidates.json"
+
+const data = JSON.parse(JSON.stringify(candidates))
 
 export default {
-  /*
-  candidates: {
-    list: (): Promise<Candidate[]> => Promise.resolve([]),
-  },
-
-  */
-
-  list: (): Promise<Candidate[]> =>
-    fetch(candidates, {
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-    })
-      .then((res) => {
-        return res.json()
-      })
-      .catch((err) => {
-        console.log("Data: ", err)
-      }),
+  list: (): Promise<Candidate[]> => Promise.resolve(data),
 }
